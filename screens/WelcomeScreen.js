c316f2bab7b086870cf191c58100833001b4f185
MaGeek32 <sfen0011@gmail.com>
2022-06-18 18:57:38 +1000

    Initial commit

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View } from 'react-native'
+import axios from 'axios'
+import { useContext, useEffect } from 'react'
+import { useState } from 'react'
+import AuthContent from '../components/Auth/AuthContent'
+import { AuthContext } from '../store/auth-context'
+
+function WelcomeScreen () {
+  const [fetchedMessage, setFetchedMessage] = useState('')
+
+  const authCtx = useContext(AuthContext)
+  const token = authCtx.token
+
+  useEffect(() => {
+    axios.get('https://userlogin-react-native-default-rtdb.firebaseio.com/message.json?auth='
+      + token).then((response) => {
+        setFetchedMessage(response.data)
+      })
+  }, [token])
+  return (
+    <View style={styles.rootContainer}>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
+    </View>
+  )
+}
+
+export default WelcomeScreen
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 32,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+})
